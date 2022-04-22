86c648ac6daeb39d97fd188d97326ef0d67aac5a
David Lazaridis <40458315+dclaz@users.noreply.github.com>
2022-04-22 21:51:47 +1000

    first real commit

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' AFL match results and betting odds
+#'
+#' A dataframe contianing AFL match results and betting odds since mid 2009.
+#'
+#' @format A data frame with 2534 rows and 12 code
+"afl_matches_df"
